08b475143ee7c6432b24fee626b67840676c9fbc
Gareth Dsouza <gdsouza@usc.edu>
2016-10-01 22:05:56 -0700

    Created cards and need to fix responsive layout

---
+++ phone-list.component.js
@@ -0,0 +1,15 @@
+'use strict';
+// Register `phoneList` component, along with its associated controller and template
+angular.
+  module('phoneList').
+  component('phoneList', {
+    templateUrl: 'phone-list/phone-list.template.html',
+    controller: ['$http', function PhoneListController($http) {
+      var self = this;
+      self.orderProp = 'age';
+
+      $http.get('projects.json').then(function(response) {
+        self.phones = response.data;
+      });
+    }]
+  });
